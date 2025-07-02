1dd9b2a800c312520e59e5273b1fd60b46c9e8a0
Hamad Alaslani <hamadsmma@gmail.com>
2025-07-03 02:05:35 +0300

    Remove game objects when they are out of bounds

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class OutOfBounds : MonoBehaviour
 {
     private float rangeZ = 2.1f;
+    private float rangeX = 6.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,7 @@ public class OutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Keep player within bounds
         if (transform.position.z > rangeZ)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, rangeZ);
@@ -19,6 +21,16 @@ public class OutOfBounds : MonoBehaviour
         if (transform.position.z < -rangeZ)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -rangeZ);
+        }
+
+        // Destroy objects out of bounds
+        if (transform.position.x > rangeX)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.x < -rangeX)
+        {
+            Destroy(gameObject);
         }
     }
 }
